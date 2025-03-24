47a212096156162a545edaf83a2194f36e069f0d
jmarcotte227 <95249839+jmarcotte227@users.noreply.github.com>
2025-03-24 13:43:40 -0400

    single line intersected by a plane

--- nuh_slicing.py
+++ nuh_slicing.py
@@ -65,7 +65,26 @@ class nuhSlicer:
             axes.set_aspect('equal')
             plt.show()
 
+    def _intersect_line_plane(self,line,plane, epsilon = 1e-6):
+        '''
+        Calculates the intersection point of a line segment and a plane.
+        If the intersection isn't between the two points on the line, then retursn None
+        line: np array (2,3). Row: point, Col: x,y,z coords
+        plane: plane of the form ax+by+cz+d=0
+        Math taken from https://stackoverflow.com/questions/5666222/3d-line-plane-intersection
+        '''
+        u =line[1,:]-line[0,:]
+        dot = u.dot(plane[:3])
 
+        if abs(dot) > epsilon:
+            p_co = plane[:3]*(-plane[3]/plane[:3].dot(plane[:3]))
+            w = line[0,:]-p_co
+            fac = -plane[:3].dot(w)/dot
+            print(fac)
+            u = u*fac
+            return line[0,:]+u
+
+        return None
 
 if __name__=='__main__':
     stl_file = 'models/funnel_tube_solid.stl'
@@ -79,16 +98,26 @@ if __name__=='__main__':
         ])
 
     # define the parameters of the plane
-    normal = np.array([1, 0, 1])
+    normal = np.array([0, 0, 1])
     normal = normal/np.linalg.norm(normal)
-    print(normal)
-    dist = 30
+    dist = -0.5
     plane = np.zeros(4)
     plane[:3] = normal
     plane[3] = dist
     
     slicer = nuhSlicer(stl_file, tf)
-    slicer.plane_cut(plane, vis=True)
+    # slicer.plane_cut(plane, vis=True)
+    line = np.array([[1,1,1],[-1, -1, -1]])
+    point = slicer._intersect_line_plane(line, plane)
+    
+    xx, yy = np.meshgrid(range(-2,2), range(-2, 2))
+    z = (-plane[0]*xx-plane[1]*yy-plane[3])/plane[2]
+    fig = plt.figure()
+    axes = fig.add_subplot(projection='3d')
+    axes.plot_surface(xx,yy,z, alpha=0.2)
+    axes.plot3D(line[:,0], line[:,1], line[:,2])
+    axes.scatter(point[0], point[1], point[2], c='r')
+    plt.show()
 
 
     # plotting to test
